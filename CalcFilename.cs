a575574c001e0d6753b52f471b6659bc7cf254cf
unknown <ren.auxo@qq.com>
2020-03-31 16:30:14 +0800

    加入專案檔案。

---
+++ CalcFilename.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SmallTools
+{
+    class CalcFilename
+    {
+        public double total { get; }
+
+        public CalcFilename(string dirPath)
+        {
+            try
+            {
+                // 建立指定目錄的實例
+                DirectoryInfo dir = new DirectoryInfo(dirPath);
+                // 傳回指定目錄中所有檔案清單
+                FileInfo[] fileInfo = dir.GetFiles();
+                // 遍歷所有檔案，獲取指定的檔案名稱
+                foreach (var fileName in fileInfo)
+                {
+                    string pattern = @"([1-9]\d*\.?\d*)";
+
+                    foreach (Match match in Regex.Matches(fileName.Name, pattern))
+                    {
+                        double number = 0.0;
+                        Double.TryParse(match.Value, out number);
+                        total += number;
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SmallTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            while (true)
+            {
+                Console.WriteLine("請選擇功能：");
+                Console.WriteLine("1 | 計算資料夾檔名中的數字");
+                Console.Write("\n請輸入： ");
+                string input = Console.ReadLine();
+                if (input.Trim().ToUpper() == "EXIT") break; // 結束程式
+                int select;
+                int.TryParse(input, out select);
+
+                switch (select)
+                {
+                    case 1: // 進入CalcFilename
+                        FunCalcFilename();
+                        break;
+                    default:
+                        break;
+
+
+                }
+
+                Console.WriteLine("");
+            }
+        }
+
+        static void FunCalcFilename()
+        {
+            while (true)
+            {
+                Console.WriteLine("\n[資料夾檔名計算功能]");
+                Console.WriteLine("請輸入資料夾路徑： (輸入exit退出)");
+                Console.Write("請輸入： ");
+                string input = Console.ReadLine();
+                if (input.Trim().ToUpper() == "EXIT") break;
+
+                if (input != "")
+                {
+                    CalcFilename calcFilename = new CalcFilename(input);
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine($"總金額為￥ {calcFilename.total} 元");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+
+
+            }
+        }
+    }
+}
